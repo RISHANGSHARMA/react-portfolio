5467c0b9cf948b1ec4d3ab7ed708665968c590b4
RISHANGSHARMA <rishangsharma@gmail.com>
2024-06-03 02:45:08 -0400

    just need to add projects

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,28 @@
-import React from "react";
-
+/* Add this useEffect to Hero.jsx */
+import React, { useEffect } from "react";
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+  useEffect(() => {
+    const title = document.querySelector(`.${styles.title}`);
+    const description = document.querySelector(`.${styles.description}`);
+
+    const titleTypingDuration = 3000; // Duration of title typing animation in milliseconds
+    const descriptionDelay = titleTypingDuration + 500; // Delay before showing description
+
+    // Set a timeout to show the description after the title animation
+    setTimeout(() => {
+      description.classList.add(styles.show);
+    }, descriptionDelay);
+  }, []);
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Rishang Sharma</h1>
         <p className={styles.description}>
-        Cloud & DevOps Engineer
-with Specialization
+          Cloud & DevOps Engineer with Specialization in Cyber Sec
         </p>
         <a href="mailto:rishangsharma@email.com" className={styles.contactBtn}>
           Contact Me
